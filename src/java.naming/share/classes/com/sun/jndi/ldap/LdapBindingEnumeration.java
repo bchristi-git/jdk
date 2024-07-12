aef70b376e91a998768fdf6ad3a0092b43d1ad94
bchristi <brent.christian@oracle.com>
2024-07-12 09:56:41 -0700

    merge

--- src/java.naming/share/classes/com/sun/jndi/ldap/LdapBindingEnumeration.java
+++ src/java.naming/share/classes/com/sun/jndi/ldap/LdapBindingEnumeration.java
@@ -33,6 +33,7 @@ import java.util.Vector;
 import javax.naming.*;
 import javax.naming.directory.*;
 import javax.naming.ldap.Control;
+import java.lang.ref.Reference;
 
 import com.sun.jndi.toolkit.ctx.Continuation;
 import com.sun.naming.internal.NamingManagerHelper;
@@ -55,51 +56,54 @@ final class LdapBindingEnumeration
     protected Binding
       createItem(String dn, Attributes attrs, Vector<Control> respCtls)
         throws NamingException {
-
-        Object obj = null;
-        String atom = getAtom(dn);
-
-        if (attrs.get(Obj.JAVA_ATTRIBUTES[Obj.CLASSNAME]) != null) {
-            // serialized object or object reference
-            try {
-                PrivilegedExceptionAction<Object> pa = () -> Obj.decodeObject(attrs);
-                obj = AccessController.doPrivileged(pa, acc);
-            } catch (PrivilegedActionException e) {
-                throw (NamingException)e.getException();
-            }
-        }
-        if (obj == null) {
-            // DirContext object
-            obj = new LdapCtx(homeCtx(), dn);
-        }
-
-        CompositeName cn = new CompositeName();
-        cn.add(atom);
-
         try {
+            Object obj = null;
+            String atom = getAtom(dn);
+
+            if (attrs.get(Obj.JAVA_ATTRIBUTES[Obj.CLASSNAME]) != null) {
+                // serialized object or object reference
+                try {
+                    PrivilegedExceptionAction<Object> pa = () -> Obj.decodeObject(attrs);
+                    obj = AccessController.doPrivileged(pa, acc);
+                } catch (PrivilegedActionException e) {
+                    throw (NamingException) e.getException();
+                }
+            }
+            if (obj == null) {
+                // DirContext object
+                obj = new LdapCtx(homeCtx(), dn);
+            }
+
+            CompositeName cn = new CompositeName();
+            cn.add(atom);
+
+            try {
             obj = NamingManagerHelper.getDirObjectInstance(obj, cn, homeCtx,
                     homeCtx.envprops, attrs, ObjectFactoriesFilter::checkLdapFilter);
 
-        } catch (NamingException e) {
-            throw e;
+            } catch (NamingException e) {
+                throw e;
 
-        } catch (Exception e) {
-            NamingException ne =
-                new NamingException(
-                        "problem generating object using object factory");
-            ne.setRootCause(e);
-            throw ne;
-        }
+            } catch (Exception e) {
+                NamingException ne =
+                    new NamingException(
+                            "problem generating object using object factory");
+                ne.setRootCause(e);
+                throw ne;
+            }
 
-        Binding binding;
-        if (respCtls != null) {
-           binding = new BindingWithControls(cn.toString(), obj,
-                                homeCtx().convertControls(respCtls));
-        } else {
-            binding = new Binding(cn.toString(), obj);
+            Binding binding;
+            if (respCtls != null) {
+                binding = new BindingWithControls(cn.toString(), obj,
+                                     homeCtx().convertControls(respCtls));
+            } else {
+                binding = new Binding(cn.toString(), obj);
+            }
+            binding.setNameInNamespace(dn);
+            return binding;
+        } finally {
+            Reference.reachabilityFence(this);
         }
-        binding.setNameInNamespace(dn);
-        return binding;
     }
 
     @Override
